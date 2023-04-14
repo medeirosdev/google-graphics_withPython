0de75a85e59f7264ded75c858ca6cd471b035440
devmedeiros <guilherme.ggcm20@gmail.com>
2023-04-14 13:56:48 -0300

    Iniciando Projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# google-graphics_withPython
---
+++ matplotlib.py
@@ -0,0 +1,10 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import datetime
+import json
+import io
+import urllib, base64
+import os
+import sys
